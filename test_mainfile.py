98171fd6703c7f656cc6d4b4a169226356e9ad6f
“rudreshrm05” <rudramadhu0509@gmail.com>
2018-07-21 13:27:32 +0530

    new test file

--- test_mainfile.py
+++ test_mainfile.py
@@ -1,4 +1,18 @@
 import demo_main_file
 import os
 
-demo_main_file.create(key="apikey", token="token", board_name="phone name", org_name="org", list_name=os.environ['PARAM_CUSTOMER'], card_name=os.environ['BUILD-DISPLAY-NAME'], card_description=os.environ['CARD_DESC'], card_comment="jenkins url")
+initialize(apikey="yourkey", token="your-token", board_name="boardname", org_name="organization name")#you only need to initialize your api, token, bname and org name once
+
+new_list(list_name"List 1")
+
+new_card(card_name="Card1", list_name="List 1", desc="new card")
+
+add_comment(card_name="Card1", comment="iron man")
+
+add_comment(card_name="Card1", comment="Captain america")
+
+new_card(card_name="Card2", list_name="List 1", desc="new card")
+
+add_comment(card_name="Card2", comment="Super man")
+
+
